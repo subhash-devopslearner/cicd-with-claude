f441047e8dcebefe3b49760158b566e87af141c9
Subhash Bhalerao <subhash-devopslearner@gmail.com>
2026-04-06 16:16:35 +0530

    Update views.py to show environment used

--- cicdproject/home/views.py
+++ cicdproject/home/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+import os
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the home page!")
+    ENV = os.getenv('ENVIRONMENT', 'development')
+    return HttpResponse(f"Welcome to the home page! Environment: {ENV}")
