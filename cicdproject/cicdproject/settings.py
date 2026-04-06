d92e7e40f16f43da4d9a06ca4ca77adfd337b9e6
Subhash Bhalerao <subhash-devopslearner@gmail.com>
2026-04-06 11:15:12 +0530

    Change DB settings for SQLite and Postgres swicthing

--- cicdproject/cicdproject/settings.py
+++ cicdproject/cicdproject/settings.py
@@ -74,14 +74,8 @@ WSGI_APPLICATION = 'cicdproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
+if os.getenv('DB_ENGINE', 'SQLite').lower() == 'postgresql':
+    DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME", "mydatabase"),
@@ -91,6 +85,16 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            #'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+
+
 
 # Cache backend
 CACHES = {
